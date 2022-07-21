99f6b83033683991657fc2337c04f3da619a109e
Tan Lai An <tlai-an@u81z01s06.42kl.edu.my>
2022-07-21 15:54:07 +0800

    21/7 work

--- tosubmit/libft/ft_strtrim.c
+++ tosubmit/libft/ft_strtrim.c
@@ -6,19 +6,19 @@
 /*   By: tlai-an <tlai-an@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 08:34:42 by tlai-an           #+#    #+#             */
-/*   Updated: 2022/07/20 08:34:42 by tlai-an          ###   ########.fr       */
+/*   Updated: 2022/07/21 15:53:28 by tlai-an          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char ft_strtrim(const char *s1, const char *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	len;
 
 	len = ft_strlen(s1);
 	if (!s1 || !set)
-		return (0);	
+		return (0);
 	if (len == 0)
 		return (ft_strdup(""));
 	while (*s1 && ft_strchr(set, *s1))
@@ -31,4 +31,3 @@ char ft_strtrim(const char *s1, const char *set)
 		len--;
 	return (ft_substr(s1, 0, (len + 1)));
 }
-
