bd218e7fc83d51a668ca99034dd9ec7e0558154f
Lai An <tanlaian2004@gmail.com>
2022-08-03 15:08:47 +0800

    3/8 work

--- prototypes/ft_printf proto/ft_printf_main.c
+++ prototypes/ft_printf proto/ft_printf_main.c
@@ -2,10 +2,26 @@
 
 int ft_printf(const char *format, ...)
 {
-	va_list arglist;
+	va_list	arglist;
+	int		i;
 
+	ft_striter(format, i); //strchr to find % (?)
 }
 
-// a - print and check for % while printing
+//print and check for % while printing
 
 
+char *ft_striter(const char *format, int index) // put results in array and return 
+{
+	char *result;
+
+	if(!format[index])
+		return (0);
+	if (format[index] == '%' && format[index + 1] != '\0')
+	{
+
+	}
+}
+
+//	flags: +, ,#
+//	conversion specifiers: c,s,p,d,i,u,x,X,%
