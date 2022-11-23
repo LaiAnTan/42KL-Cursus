3ba512c23e96ab80b47dd697d8af583d5d0ad534
laian <tanlaian2004@gmail.com>
2022-11-23 11:39:54 +0800

    done small sort

--- prototypes/push_swap proto/errors.c
+++ prototypes/push_swap proto/errors.c
@@ -2,18 +2,13 @@
 
 int	error(int code, t_stack *stack)
 {
-	if (code == 1)
-		printf("Error, Arguement not an integer\n");
-	else if (code == 2)
-		printf("Error, Arguement overflow\n");
-	else if (code == 3)
-		printf("Error, Duplicate arguements found\n");
+	printf("Error\n");
 	deinitstack(stack);
 	exit(code);
 	return (0);
 }
 
-int	check_duplicate(t_stack *stack)
+void	check_duplicate(t_stack *stack)
 {
 	int	i;
 	int	j;
@@ -25,15 +20,18 @@ int	check_duplicate(t_stack *stack)
 		while (j < stack ->size_a)
 		{
 			if (i == j)
-			{
 				j++;
-				continue ;
-			}
 			if (stack ->stack_a[i] == stack ->stack_a[j])
-				return (error(3, stack));
-			j++;
+			{
+				error(3, stack);
+				return ;
+			}
+			else
+				j++;
 		}
 		i++;
 	}
-	return (0);
+	return ;
 }
+
+// duplicates in the middle are not found
