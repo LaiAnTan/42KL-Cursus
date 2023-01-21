a30a85d2980160e365fefc3de70249f1a856d9ad
laian <tanlaian2004@gmail.com>
2023-01-21 11:01:55 +0800

    stack v heap

--- prototypes/cpp/cpp01/ex00/newZombie.cpp
+++ prototypes/cpp/cpp01/ex00/newZombie.cpp
@@ -1,5 +1,18 @@
 #include "Zombie.hpp"
 
+/*
+When a variable (primarily objects) is dynamically allocated memory 
+using the new keyword, the memory is allocated on the heap.
+
+The constructor of the object is also called to initialize the object.
+
+The object can be returned and use outside of the scope of the function.
+
+When the object is no longer used, the memory must be freed
+using the delete keyword.
+
+*/
+
 Zombie*	newZombie(string name)
 {
 	Zombie* newZombie = new Zombie();
--- prototypes/cpp/cpp01/ex00/randomChump.cpp
+++ prototypes/cpp/cpp01/ex00/randomChump.cpp
@@ -1,5 +1,18 @@
 #include "Zombie.hpp"
 
+/*
+When a variable is created, it is allocated on the stack.
+
+Allocating on the stack is faster than heap allocation because
+memory is allocated and deallocated more quickly on the stack.
+
+However, stack size is limited and the program may fail or crash if too many
+variables are allocated on the stack.
+
+This variable "randomChump" will be destructed and freed once the function
+exits.
+*/
+
 void	randomChump(string name)
 {
 	Zombie randomChump;
