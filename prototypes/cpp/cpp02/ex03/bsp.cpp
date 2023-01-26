11afa72ff7ab37b40cb2e4ac6688aee1a3d579f0
laian <tanlaian2004@gmail.com>
2023-01-26 17:16:47 +0800

    bsp

---
+++ prototypes/cpp/cpp02/ex03/bsp.cpp
@@ -0,0 +1,14 @@
+#include "Point.hpp"
+
+float area(Point const a, Point const b, Point const c)
+{
+
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point)
+{
+
+	float area1;
+	float area2;
+	float area3;
+}
