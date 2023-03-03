729b7217e982447464064de01b01b1eb1744a57e
laian <tanlaian2004@gmail.com>
2023-03-03 09:34:23 +0800

    it works wow

--- exam practice/exam03/get_next_line/get_next_line.c
+++ exam practice/exam03/get_next_line/get_next_line.c
@@ -1,32 +1,153 @@
 #include "get_next_line.h"
 
+#include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+int		ft_strlen(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int		has_nl(char *str, int size)
+{
+	int	i = 0;
+	
+	while (i < size && str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*append(char *src, char *dest)
+{
+	int		i = 0;
+	int		j = 0;
+	int		size;
+	char	*str;
+	
+	str = malloc(sizeof(char) * (ft_strlen(src) + ft_strlen(dest) + 1));
+	if (dest != NULL)
+	{
+		while (dest[i] != '\0')
+		{
+			str[i] = dest[i];
+			i++;
+		}
+	}
+	if (src != NULL)
+	{
+		while (src[j] != '\0')
+		{
+			str[i] = src[j];
+			i++;
+			j++;
+		}
+	}
+	str[i] = '\0';
+	free(dest);
+	return (str);
+}
+
+char	*get_nl_str(char *line)
+{
+	int		i = 0;
+	int		j = has_nl(line, ft_strlen(line));
+	char	*str;
+
+	if (!line)
+		return (NULL);
+	str = malloc (sizeof(char) * (j + 1));
+	while (line[i] != '\n' && line[i] != '\0')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	str[i] = '\n';
+	str[i + 1] = '\0';
+	return (str);
+}
+
+char	*skip(char *line)
+{
+	int		j = 0;
+	int		i  = has_nl(line, ft_strlen(line));
+	char	*str;
+
+	if (i == -1)
+		return (NULL); //no ln found
+	else
+		i++; // to skip \n
+		
+	str = malloc(sizeof(char) * (ft_strlen(line) - i + 1));
+	while (i < ft_strlen(line))
+	{
+		str[j] = line[i];
+		j++;
+		i++;
+	}
+	str[j] = '\0';
+	free(line);
+	return (str);
+}
+
 char	*get_next_line(int fd)
 {
-	char buf[BUFFER_SIZE];
+	int			bytes = 0;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*ret_str = NULL;
+	static char *line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	read(fd, &buf, BUFFER_SIZE);
+	while (has_nl(buffer, bytes) == -1)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (!bytes || bytes == -1)
+			break;
+		buffer[bytes] = '\0';
+		line = append(buffer, line);
+	}
+
+	if (!line)
+		return (NULL);
+	ret_str = get_nl_str(line);
+	if (!ret_str)
+		return (NULL);
+	line = skip(line);
+	return (ret_str);
+
 }
 
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(void)
+int main(void)
 {
-	int fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
+	int fd = open("test.txt", O_RDWR);
+	char	*line;
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	line = get_next_line(fd);
+	printf("str: %s", line);
+	free(line);
 }
-
-/*
-gnl
-check fd < 0 & buffer size <= 0
-malloc buffer
-read from file
-transfer
-
-
-*/
