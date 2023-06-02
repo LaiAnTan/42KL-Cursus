5bc4b1d4738a2f9cddf90d4b7558ab617b76147d
laian <tanlaian2004@gmail.com>
2023-06-02 14:56:30 +0800

    cpp01 ready to submit

--- tosubmit/cpp01/ex02/main.cpp
+++ tosubmit/cpp01/ex02/main.cpp
@@ -11,13 +11,13 @@ int main(void)
 	string	*stringPTR = &str;
 	string	&stringREF = str;
 
-	cout << "Memory address of variable str: " << &str << endl;
-	cout << "Memory address of pointer to variable str:" << &stringPTR << endl;
-	cout << "Memory address of reference to variable str:" << &stringREF << endl;
+	cout << "Memory address of variable str:\t\t\t" << &str << endl;
+	cout << "Memory address of pointer to variable str:\t" << &stringPTR << endl;
+	cout << "Memory address of reference to variable str:\t" << &stringREF << endl;
 	cout << endl;
-	cout << "Value of variable str: " << str << endl;
-	cout << "Value pointed by stringPTR: " << *stringPTR << endl;
-	cout << "Value pointed by stringREF: " << stringREF << endl;
+	cout << "Value of variable str:\t\t\t\t" << str << endl;
+	cout << "Value pointed by stringPTR:\t\t\t" << *stringPTR << endl;
+	cout << "Value pointed by stringREF:\t\t\t" << stringREF << endl;
 }
 
 /*
--- tosubmit/cpp01/ex05/main.cpp
+++ tosubmit/cpp01/ex05/main.cpp
@@ -1,11 +1,23 @@
 #include "Harl.hpp"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 
 int	main(void)
 {
 	Harl	harl;
 
+	cout << "[DEBUG]" << endl;
 	harl.complain("DEBUG");
+
+	cout << "[INFO]" << endl;
 	harl.complain("INFO");
+
+	cout << "[WARNING]" << endl;
 	harl.complain("WARNING");
+
+	cout << "[ERROR]" << endl;
 	harl.complain("ERROR");
 }
