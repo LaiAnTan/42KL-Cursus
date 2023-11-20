960e7c2f15b9483f3c8c5572bb392ef0c48aac7a
laian <tanlaian2004@gmail.com>
2023-11-20 16:23:08 +0800

    fix: cleanup

--- 6th_Circle/cpp05/cpp05_final/ex03/main.cpp
+++ 6th_Circle/cpp05/cpp05_final/ex03/main.cpp
@@ -12,34 +12,37 @@ using std::endl;
 
 int main(void)
 {
+	Bureaucrat	*workerA = new Bureaucrat("William", 5);
+
+	Intern		*internA = new Intern();
+
+	AForm		*formA = internA->makeForm("presidential pardon", "bob");
+	AForm		*formB = internA->makeForm("robotomy request", "elon musk");
+	AForm		*formC = internA->makeForm("shrubbery creation", "mars");
+
 	try
 	{
-		Bureaucrat	*workerA = new Bureaucrat("William", 5);
-
-		Intern		*internA = new Intern();
-
-		AForm		*formA = internA->makeForm("presidential pardon", "bob");
-		AForm		*formB = internA->makeForm("robotomy request", "elon musk");
-		AForm		*formC = internA->makeForm("shrubbery creation", "mars");
 		AForm		*formThatDoesNotExist = internA->makeForm("sus", "amogus");
-
-		cout << *workerA << endl;
-
-		cout << *formA << endl;
-		cout << *formB << endl;
-		cout << *formC << endl;
-
-		delete workerA;
-		delete internA;
-
-		delete formA;
-		delete formB;
-		delete formC;
 		delete formThatDoesNotExist;
 	}
-	catch(const std::exception& err)
+	catch (const Intern::FormNotFoundException& err)
 	{
 		std::cerr << err.what() << endl;
+		
 	}
+
+	cout << *workerA << endl;
+
+	cout << *formA << endl;
+	cout << *formB << endl;
+	cout << *formC << endl;
+
+	delete workerA;
+	delete internA;
+
+	delete formA;
+	delete formB;
+	delete formC;
+
 	return (0);
 }
